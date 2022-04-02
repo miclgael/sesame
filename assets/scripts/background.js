ca31e652e9de7c1ea4955155b7887b500ad741e1
Michael Gale <michael@michaelgale.dev>
2022-04-02 12:43:25 +1100

    chore: linting

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -1,44 +1,50 @@
-const contexts = ['localhost', 'staging', 'www'];
+const contexts = ["localhost", "staging", "www"];
 
 /**
  * Create a context menu items (right click on any tab)
- * @param {Array} contexts 
+ * @param {Array} contexts
  */
 // function createMenuItems(contexts) {
-    contexts.forEach(label => {
-        browser.menus.create({
-            id: `menu-item-${label}`,
-            title: `Reopen as ${label}`,
-            contexts: ["tab"]
-        }, onCreated);
-    });
+contexts.forEach((label) => {
+  browser.menus.create(
+    {
+      id: `menu-item-${label}`,
+      title: `Reopen as ${label}`,
+      contexts: ["tab"],
+    },
+    onCreated
+  );
+});
 
-     /* Also add a link to the Options page */
-     browser.menus.create({
-         id: `menu-item-options`,
-         title: `   Options`,
-         contexts: ["tab"]
-     }, onCreated);
+/* Also add a link to the Options page */
+browser.menus.create(
+  {
+    id: `menu-item-options`,
+    title: `   Options`,
+    contexts: ["tab"],
+  },
+  onCreated
+);
 // }
 
 function onCreated() {
-    if (browser.runtime.lastError) {
-        console.log(`Error: ${browser.runtime.lastError}`);
-    } else {
-        console.log("Item created successfully");
-    }
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
 }
 
 function onRemoved() {
-    console.log("Item removed successfully");
+  console.log("Item removed successfully");
 }
 
 function onError(error) {
-    console.log(`Error: ${error}`);
+  console.log(`Error: ${error}`);
 }
 
 function identifyCurrentTab(tab) {
-    console.log(tab.url)
+  console.log(tab.url);
 }
 
 /**
@@ -47,8 +53,8 @@ function identifyCurrentTab(tab) {
  * @param {String} newOrigin - new origin / base
  */
 function swapBaseUrl(url, newOrigin) {
-    url = new URL(url); // allow use of window.location methods
-    return newOrigin + url.href.split(url.origin)[1] 
+  url = new URL(url); // allow use of window.location methods
+  return newOrigin + url.href.split(url.origin)[1];
 }
 
 /*
@@ -56,56 +62,48 @@ The click event listener, where we perform the appropriate action given the
 ID of the menu item that was clicked.
 */
 browser.menus.onClicked.addListener((info, tab) => {
+  function onError(e) {
+    console.log(`eee Error: ${e}`);
+  }
+  var getting = browser.storage.sync.get();
 
-    function onError(e) {
-        console.log(`eee Error: ${e}`)
-    }
-    var getting = browser.storage.sync.get();
+  switch (info.menuItemId) {
+    case "menu-item-localhost":
+      console.log(tab);
 
-    switch (info.menuItemId) {
-        case "menu-item-localhost":
-           
-            console.log(tab);
+      getting.then((result) => {
+        browser.tabs.update(tab.id, {
+          url: swapBaseUrl(tab.url, result.localhost),
+        });
+      }, onError);
 
-            getting.then(result => {
-                
-                browser.tabs.update(tab.id, {
-                    url: swapBaseUrl(tab.url, result.localhost)
-                })
-                
-            }, onError);
-       
-            break;
-        case "menu-item-staging":
-            
-            getting.then(result => {
-                 browser.tabs.update(tab.id, {
-                     url: swapBaseUrl(tab.url, result.staging)
-                 })
-            }, onError);
+      break;
+    case "menu-item-staging":
+      getting.then((result) => {
+        browser.tabs.update(tab.id, {
+          url: swapBaseUrl(tab.url, result.staging),
+        });
+      }, onError);
 
-            break;
-        case "menu-item-www":
+      break;
+    case "menu-item-www":
+      getting.then((result) => {
+        browser.tabs.update(tab.id, {
+          url: swapBaseUrl(tab.url, result.www),
+        });
+      }, onError);
 
-            getting.then(result => {
-                browser.tabs.update(tab.id, {
-                    url: swapBaseUrl(tab.url, result.www)
-                })
-            }, onError);
+      break;
+    case "menu-item-options":
+      function onOpened() {
+        console.log(`Options page opened`);
+      }
+      function onError() {
+        console.log(`Options page error`);
+      }
+      var opening = browser.runtime.openOptionsPage();
+      opening.then(onOpened, onError);
 
-        break;
-        case "menu-item-options":
-            
-            function onOpened() {
-                console.log(`Options page opened`);
-            }
-            function onError() {
-                console.log(`Options page error`);
-            }
-            var opening = browser.runtime.openOptionsPage();
-            opening.then(onOpened, onError);
-            
-        break;
-    }
-
+      break;
+  }
 });
--- assets/scripts/options.js
+++ assets/scripts/options.js
@@ -1,42 +1,40 @@
 /* Store references to the form inputs on the options page */
 const FormInputLocal = document.getElementById("localhost");
 const FormInputStaging = document.getElementById("staging");
-const FormInputProduction =  document.getElementById("www");
+const FormInputProduction = document.getElementById("www");
 
 /**
  * Save current values of the form into local storage object
  * @param {*} e - event object
  */
 function saveOptions(e) {
-    e.preventDefault();
-    browser.storage.sync.set({
-        localhost: FormInputLocal.value,
-        staging: FormInputStaging.value,
-        www: FormInputProduction.value
-    });
+  e.preventDefault();
+  browser.storage.sync.set({
+    localhost: FormInputLocal.value,
+    staging: FormInputStaging.value,
+    www: FormInputProduction.value,
+  });
 }
 
 /**
  * Fetch from the local storage object and apply values back to the form
  */
 function restoreOptions() {
+  // 1. Set defaults if there's nothing in the local storage yet
+  function setCurrentChoice(result) {
+    FormInputLocal.value = result.localhost || "http://localhost:8888";
+    FormInputStaging.value = result.staging || "https://staging.yourdomain.com";
+    FormInputProduction.value = result.www || "https://www.yourdomain.com";
+  }
 
-    // 1. Set defaults if there's nothing in the local storage yet
-    function setCurrentChoice(result) {
-        FormInputLocal.value = result.localhost || "http://localhost:8888";
-        FormInputStaging.value = result.staging || "https://staging.yourdomain.com";
-        FormInputProduction.value = result.www  || "https://www.yourdomain.com";
-    }
+  // 2. Print an error message to `about:devtools-toolbox`
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
 
-    // 2. Print an error message to `about:devtools-toolbox`
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
-
-    // 3. Get and set.
-    var getting = browser.storage.sync.get();
-        getting.then(setCurrentChoice, onError);
-
+  // 3. Get and set.
+  var getting = browser.storage.sync.get();
+  getting.then(setCurrentChoice, onError);
 }
 
 /* Handle events here */
